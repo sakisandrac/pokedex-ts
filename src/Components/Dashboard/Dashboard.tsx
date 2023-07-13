9e3548926e534daacd9e80832a1c8ba92c955b82
“Saki <sakisandra.c@gmail.com>
2023-07-12 22:50:54 -0600

    Add types to random amd saved pokemon

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -2,45 +2,7 @@ import React, { useState } from 'react';
 import './Dashboard.css';
 import RandomPokemon from '../RandomPokemon/RandomPokemon';
 import Details from '../Details/Details';
-
-
-interface AllPokemonI {
-  name: string,
-  url: string
-}
-
-interface AbilitiesI {
-  ability: AllPokemonI
-  is_hidden: boolean,
-  slot: number
-}
-
-interface TypesI {
-  slot: number,
-  type: AllPokemonI[]
-}
-
-interface MovesI {
-  move: AllPokemonI
-  version_group_details: any[]
-}
-
-interface CleanPokeDataI {
-  abilities:  AbilitiesI[]
-  id: number,
-  name: string,
-  types: TypesI[],
-  moves: MovesI[],
-  weight: number
-}
-
-interface SavedPokemonI {
-  call: boolean,
-  data: CleanPokeDataI,
-  image: string,
-  number: number,
-  showDetails: boolean
-}
+import { SavedPokemonI } from '../../Types/Types';
 
 interface DashboardProps {
   savePokemon: () => void,
